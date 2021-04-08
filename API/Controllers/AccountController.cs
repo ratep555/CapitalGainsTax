743bbe56f2567ae1a5a4d4eeb6ab1a495d4fce9f
peki555 <52975990+peki555@users.noreply.github.com>
2021-04-08 20:03:40 +0200

    End of Section 10

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,3 @@
-using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
@@ -43,7 +42,7 @@ namespace API.Controllers
             };
         }
         [HttpGet("emailexists")]
-        public async Task<ActionResult<bool>> CheckEmailExists([FromQuery]string email)
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery]string email)
         {
              return await _userManager.FindByEmailAsync(email) != null;
         }
@@ -68,6 +67,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+             if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult
+                (new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
